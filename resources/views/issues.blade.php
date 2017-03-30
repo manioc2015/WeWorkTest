bf90cb1ef5784f1452f76edf3bc11425121e4f8f
Chung Lau <manioc@gmail.com>
2017-03-30 21:08:29 +0000

    fixed issue listing

--- resources/views/issues.blade.php
+++ resources/views/issues.blade.php
@@ -9,9 +9,9 @@
                 <div class="panel-body" id="issues">
 			@if (count($issues) > 0)
 			<table style="width: 100%">
-				<tr><th>ID</th><th>Name</th><th>Owner</th><th>Issues</th></tr>
+				<tr><th>#</th><th>User</th><th>Title</th><th>Issue</th></tr>
 				@foreach ($issues as $issue)
-				<tr><td>{{$repo->id}}</td><td>{{$repo->name}}</td><td>{{$repo->owner->login}}</td><td><a href='/showRepoIssues/{{$repo->owner->login}}/{{$repo->name}}'>Issues</a></td></tr>
+				<tr><td>{{$issue->number}}</td><td>{{$issue->user->login}}</td><td>{{$issue->title}}</td><td>{{$issue->body}}</td></tr>
 				@endforeach
 			</table>
 			@else
